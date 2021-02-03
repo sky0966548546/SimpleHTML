d39bdf2ba53c6227a7c6f41fd83ca10ddd9100c4
oF <60073419+sky0966548546@users.noreply.github.com>
2021-02-04 01:57:32 +0800

    Update 簡易網頁產生器.py
    添加可改變網頁樣式的選項

--- 簡易網頁產生器.py
+++ 簡易網頁產生器.py
@@ -1,15 +1,18 @@
 import os
 os.system('cls')
-# 基礎設置
+# 基礎設置(html)
 print('----------基礎設置----------\n')
 os.chdir('c:/Downloads')
 name = input('請輸入檔案名稱：')
-files = name + '.html'
-a = open(files, "w", encoding='utf8')
-# 網頁基礎內容
+html = name + '.html'
+chdir = 'c:/Downloads/'+name
+os.mkdir(name)
+os.chdir(chdir)
+a = open(html, "w", encoding='utf8')
+# 網頁基礎內容(body)
 title = input('請輸入網頁標題：')
 a.write("<!DOCTYPE html>\n<html lang='zh_TW'>\n<head>\n<meta charset='utf-8' />\n")
-a.write("<link rel='stylesheet' href='https://shark.bucky.tw/python.css' />\n")
+a.write("<link rel='stylesheet' href='style.css' />\n")
 title = "<title>"+title+"</title>\n</head>\n<body>\n<div class='kirito'>\n"
 a.write(title)
 print('\n----------網頁內容----------\n')
@@ -24,6 +27,23 @@ phone = '<p>我的電話：'+phone+'</p>'
 end = h1 + '\n' + h3 + '\n' + email + '\n' + phone + '\n'
 a.write(end)
 a.write('</div>\n</body>\n</html>')
+# 網頁樣式(css)
+print('\n----------網頁樣式----------\n')
+a = open('style.css', "w", encoding='utf8')
+family = input('請輸入網頁字體：')
+family = 'font-family:'+family+';\n'
+bgc1 = input('請輸入主背景色：')
+bgc1 = 'background-color:'+bgc1+';\n'+'}\n\n.kirito{\n'
+bgc2 = input('請輸入副背景色：')
+bgc2 = 'background-color:'+bgc2+';\n'
+color = input('請輸入字體顏色：')
+color = 'color:'+color+';\n}\n\n.kirito h1{\ntext-transform:uppercase;\n}'
+a.write('body{\n')
+a.write(family)
+a.write(bgc1)
+a.write(bgc2)
+a.write('width:300px;\npadding:40px;\nposition:absolute;\ntop:50%;\nleft:50%;\ntransform:translate(-50%,-50%);\n')
+a.write(color)
 print('\n----------製作完成----------\n')
 print('Enter後檔案將儲存於c:/Downloads')
 input()
